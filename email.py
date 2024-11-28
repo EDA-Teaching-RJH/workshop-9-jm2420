56566926d1b8f3ea781424074d880c92af818d2c
jm2420 <jm2420@kent.ac.uk>
2024-11-28 14:32:56 +0000

    STUFF

--- email.py
+++ email.py
@@ -1,1 +1,22 @@
-
+import re
+validemail = False
+@prob = False
+endprob = False
+charsprob = False
+#email = input("Please enter your email").strip()
+email = "a@a.gov.uk"
+if not re.search(r".+, @{1}.+", email):
+    @prob = True
+    
+if re.search(r"\.ac.uk$", email):
+    validemail = True
+    print("Valid Academic Email")
+if re.search(r"\.gov.uk$", email):
+    validemail = True
+    print("Valid Government Email")
+elif re.search(r"\.nhs.net$"):
+    validemail = True
+    print("Valid NHS Email")
+else:
+    print("error")
+if endprob = 
--- postcode.py
+++ postcode.py
@@ -1,1 +1,14 @@
+import re
 
+address = "University of Kent, Canterbury, Kent, CT2 7NT"
+place, city, county, postcode = address.split(", ", 3)
+print(postcode)
+pattern = r'[A-Z]{1,2}[0-9][0-9A-Z]?\s?[0-9][A-Z]{2}'
+
+postcode1 = re.search(pattern, address)
+
+if postcode1:
+    result = postcode1.group(0)
+    print(f"Extracted postcode: {result}")
+else:
+    print("No postcode found")
